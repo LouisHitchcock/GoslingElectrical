bdb9c1717b300f7e6b64a8679f5bb02f9a413668
Louis Hitchcock <48716762+LouisHitchcock@users.noreply.github.com>
2024-12-04 14:10:19 +0000

    Mobile Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
 
-function toggleMenu() {
-    const menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("visible");
-}
+// Toggle the hamburger menu and mobile menu visibility
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    if (mobileMenu.classList.contains('menu-visible')) {
+        mobileMenu.classList.remove('menu-visible');
+        mobileMenu.classList.add('menu-hidden');
+    } else {
+        mobileMenu.classList.remove('menu-hidden');
+        mobileMenu.classList.add('menu-visible');
+    }
+});
