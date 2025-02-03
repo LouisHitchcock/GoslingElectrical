38ad62d304963a3e4b368305657f3228a85b3d06
Louis Hitchcock <48716762+LouisHitchcock@users.noreply.github.com>
2025-02-03 18:04:15 +0000

    Improved Mobile Compatibility

--- js/script.js
+++ js/script.js
@@ -24,19 +24,3 @@ const swiper = new Swiper('.swiper-container', {
         },
     },
 });
-
-
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobile-menu');
-
-// Toggle the hamburger menu and mobile menu visibility
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    if (mobileMenu.classList.contains('menu-visible')) {
-        mobileMenu.classList.remove('menu-visible');
-        mobileMenu.classList.add('menu-hidden');
-    } else {
-        mobileMenu.classList.remove('menu-hidden');
-        mobileMenu.classList.add('menu-visible');
-    }
-});
